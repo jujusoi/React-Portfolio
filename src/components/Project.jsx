d79d43892ac9e7b9efa638572fd9d5ab3dd7e97d
jujusoi <jalxmcdonald@hotmail.com>
2023-10-29 00:59:17 +1030

    furhter built upon port

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,22 +1,26 @@
 
 import projectInfo from './data/projects.json';
+import { Link } from 'react-router-dom';
 
-console.log(projectInfo);
-
-export default function Projects() {
+export default function Projects({ increaseSize, hovered }) {
     return (
         <>
         {projectInfo.map(projects => (
-                <div style={{ width: '40%', height: '150px'}} key={projects.id}>
-                    <div style={{width: '100%', height: '100%', backgroundImage: `url(${projects.img})`, backgroundSize: 'cover'}}>
-
+            <div id='ind-proj-hold' style={{ position: 'relative', width: '35%', height: '225px', marginBottom: 20}} key={projects.id} onMouseEnter={() => increaseSize(true, projects.id)} onMouseLeave={() => increaseSize(false)}>
+                <img style={{ width: hovered === projects.id ? '101%' : '100%', height: hovered === projects.id ? '101%' : '100%'}} src={projects.img} alt={projects.description}/>
+                <div className='overlay' style={{ position: 'absolute', top: 0, left: 0, display: 'flex', width: '100%', height: '100%'}}>
+                    <div style={{paddingLeft: 20, width: '90%'}}>
+                        <Link to={projects.application} target='_blank'>
+                            <h3 className='project-head' style={{opacity: hovered === projects.id ? 100 : 0, color: 'white'}}>{projects.title}</h3>
+                            <p className='project-para' style={{opacity: hovered === projects.id ? 100 : 0, color: 'white'}}>{projects.description}</p>
+                        </Link>
                     </div>
-                    <h3>{projects.title}</h3>
-                    <p>{projects.description}</p>
-                    <img width="150px" src={projects.img} alt={projects.description}/>
-                    <a href={projects.application}>Visit the app!</a>
+                    <div>
+                        <Link to={projects.github} target='_blank' ><img width={30} src="https://uploads-ssl.webflow.com/63373991b8094c5fce83b830/635f6e95a2af5f516d8e5a75_new-tab.webp" alt="Open Project" /></Link>
+                    </div>
                 </div>
-            ))}
+            </div>
+        ))}
         </>
     )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,16 +1,28 @@
 import Projects from "../components/Project";
+import { useState } from 'react';
 
 export default function Portfolio() {
+
+    const [hovered, setHovered] = useState(null);
+
+    const increaseSize = (status, id) => {
+        if (status) {
+            setHovered(id);
+        } else {
+            setHovered(null);
+        }
+    }
+
     return (
         <>
             <section id="portfolio-section" style={{ width: '80%'}}>
                 <div id="main-port-hold">
-                    <div id="port-text-holder">
+                    <div id="port-text-holder" style={{height: '15%'}}>
                         <h2>Portfolio</h2>
                         <p>Here are some of my projects!</p>
                     </div>
-                    <div id="port-projects-holder" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap'}}>
-                        <Projects />
+                    <div id="port-projects-holder" style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap', height: '85%', padding: 10}}>
+                        <Projects increaseSize={increaseSize} hovered={hovered} />
                     </div>
                 </div>
             </section>
